77bcab7242c6dc9d344daa18d43f902e89faf01e
Fabio Silveira <fabio.eds@hotmail.com>
2024-02-10 17:22:48 -0800

    AddedJSData

---
+++ script.js
@@ -0,0 +1,42 @@
+const easternConference = [
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    }
+];
+
+const westernConference = [
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+    {
+        team: "Celtics"
+    },
+
+];
