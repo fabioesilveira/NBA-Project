8d1c39030a952a8f561195e2b3da3e0960321948
Fabio Silveira <fabio.eds@hotmail.com>
2024-02-12 22:29:36 -0800

    AddedData

--- script.js
+++ script.js
@@ -1,42 +1,141 @@
 const easternConference = [
+   
     {
-        team: "Celtics"
+        id: 1,
+        team: "76niers",
+        currentlyPlayers: ["J.Kuminga", "A.Wiggins", "D.Green", "B.Podziemski", "S.Curry", "D.Saric", "K.Thompson", "K.Looney"],
+        stadium: "Wells Fargo Center",
+        titles: {
+            nbaTitles: 7,
+            championshipsWon: [1947, 1956, 1975, 2015, 2017, 2018, 2022]
+        },
+        based: "Philadelphia - Pennsylvania"
     },
     {
-        team: "Celtics"
+        id: 2,
+        team: "Celtics",
+        currentlyPlayers: ["J.Tatum", "J.Brown", "K.Porzingis", "D.White", "J.Holiday", "A.Horford", "S.Hauser", "O.Brisset"],
+        stadium: "TD Garden",
+        titles: {
+            nbaTitles: 17,
+            championshipsWon: [1957, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1968, 1969, 1974, 1976, 1981, 1984, 1986, 2008]
+        },
+        based: "Boston - Massachusetts"
     },
     {
-        team: "Celtics"
+        id:3,
+        team: "Bulls",
+        currentlyPlayers: ["L.James", "T.Prince", "A.Davis", "D.Russell", "A.Reaves", "C.Wood", "R.Hachimura", "M.Christie" ],
+        stadium: "United Center",
+        titles: {
+            nbaTitles: 17,
+            championshipsWon: [1949, 1950, 1952, 1953, 1954, 1972, 1980, 1982, 1985, 1987, 1988, 2000, 2001, 2002, 2009, 2010, 2020]
+        },
+        based: "Chicago - Illinois"
     },
     {
-        team: "Celtics"
+        id: 4,
+        team: "Heat",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "Kaseya Center",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Miami - Florida"
     },
     {
-        team: "Celtics"
+        id: 5,
+        team: "Cavaliers",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "Rocket Mortgage FieldHouse",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Cleveland - Ohio"
     },
     {
-        team: "Celtics"
+        id: 6,
+        team: "Bucks",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "Fiserv Forum",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Milwaukee - Wisconsin"
     }
+
 ];
 
 const westernConference = [
+    
     {
-        team: "Celtics"
+        id: 7,
+        team: "Warriors",
+        currentlyPlayers: ["J.Kuminga", "A.Wiggins", "D.Green", "B.Podziemski", "S.Curry", "D.Saric", "K.Thompson", "K.Looney"],
+        stadium: "Chase Center",
+        titles: {
+            nbaTitles: 7,
+            championshipsWon: [1947, 1956, 1975, 2015, 2017, 2018, 2022]
+        },
+        based: "San Francisco - California"
     },
     {
-        team: "Celtics"
+        id: 8,
+        team: "Nuggets",
+        currentlyPlayers: ["J.Tatum", "J.Brown", "K.Porzingis", "D.White", "J.Holiday", "A.Horford", "S.Hauser", "O.Brisset"],
+        stadium: "Ball Arena",
+        titles: {
+            nbaTitles: 17,
+            championshipsWon: [1957, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1968, 1969, 1974, 1976, 1981, 1984, 1986, 2008]
+        },
+        based: "Denver - Colorado"
     },
     {
-        team: "Celtics"
+        id: 9,
+        team: "Lakers",
+        currentlyPlayers: ["L.James", "T.Prince", "A.Davis", "D.Russell", "A.Reaves", "C.Wood", "R.Hachimura", "M.Christie" ],
+        stadium: "Crypto.com Arena",
+        titles: {
+            nbaTitles: 17,
+            championshipsWon: [1949, 1950, 1952, 1953, 1954, 1972, 1980, 1982, 1985, 1987, 1988, 2000, 2001, 2002, 2009, 2010, 2020]
+        },
+        based: "Los Angeles - California"
     },
     {
-        team: "Celtics"
+        id: 10,
+        team: "Mavericks",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "American Airlines Center",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Dallas - Texas"
     },
     {
-        team: "Celtics"
+        id: 11,
+        team: "Grizzlies",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "FedExForum",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Memphis - Tennessee"
     },
     {
-        team: "Celtics"
-    },
+        id: 12,
+        team: "Clippers",
+        currentlyPlayers: ["J.Butler", "Bam Adebayo", "T.Herro", "T.Rozier", "J.Richardson", "C.Martin", "K.Love", "J.Jaquez Jr"],
+        stadium: "Crypto.com Arena",
+        titles: {
+            nbaTitles: 3,
+            championshipsWon: [2006, 2012, 2013]
+        },
+        based: "Los Angeles - California"
+    }
 
 ];
