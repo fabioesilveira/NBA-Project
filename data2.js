dffc5a182e906bafbd5e46b901978220bdea254c
Fabio Silveira <fabio.eds@hotmail.com>
2024-02-19 22:46:44 -0800

    AddeddesignandData2

---
+++ data2.js
@@ -0,0 +1,60 @@
+const easternRegularSeason = [
+
+    {
+        id: 1,
+        team: "Celtics",
+        w: 43,
+        l: 12
+
+    },
+    {
+        id: 2,
+        team: "Cavaliers",
+        w: 36,
+        l: 17
+
+    },
+    {
+        id: 3,
+        team: "Bucks",
+        w: 35,
+        l: 21
+
+    },
+    {
+        id: 4,
+        team: "Knicks",
+        w: 33,
+        l: 22
+
+    },
+    {
+        id: 5,
+        team: "76niers",
+        w: 32,
+        l: 22
+
+    },
+    {
+        id: 6,
+        team: "Pacers",
+        w: 31,
+        l: 25
+
+    },
+    {
+        id: 7,
+        team: "Heat",
+        w: 30,
+        l: 25
+
+    },
+    {
+        id: 8,
+        team: "Magic",
+        w: 30,
+        l: 25
+
+    },
+
+]
