bc76c110e587828b4ff29aa186eeac5eac75ca3a
Fabio Silveira <fabio.eds@hotmail.com>
2024-02-26 22:29:16 -0800

    addedDatabase

--- data2.js
+++ data2.js
@@ -58,3 +58,96 @@ export const easternRegularSeason = [
     },
 
 ];
+
+const westernRegularSeason = [
+    {
+        id: 1,
+        team: "Timberwolves",
+        w: 40,
+        l: 17
+    },
+    {
+        id: 2,
+        team: "Thunder",
+        w: 40,
+        l: 17
+    },
+    {
+        id: 3,
+        team: "Nuggets",
+        w: 39,
+        l: 19
+    },
+    {
+        id: 4,
+        team: "Clippers",
+        w: 37,
+        l: 19
+    },
+    {
+        id: 5,
+        team: "Suns",
+        w: 34,
+        l: 24
+    },
+    {
+        id: 6,
+        team: "Pelicans",
+        w: 34,
+        l: 24
+    },
+    {
+        id: 7,
+        team: "Kings",
+        w: 33,
+        l: 24
+    },
+    {
+        id: 8,
+        team: "Mavericks",
+        w: 33,
+        l: 24
+    },
+    {
+        id: 9,
+        team: "Lakers",
+        w: 31,
+        l: 28
+    },
+    {
+        id: 10,
+        team: "Warriors",
+        w: 29,
+        l: 27
+    },
+    {
+        id: 11,
+        team: "Jazz",
+        w: 27,
+        l: 31
+    },
+    {
+        id: 12,
+        team: "Rockets",
+        w: 25,
+        l: 32
+    },
+    {
+        id: 13,
+        team: "Grizzlies",
+        w: 20,
+        l: 38
+    },
+    {
+        id: 14,
+        team: "Trail Blazers",
+        w: 15,
+        l: 41
+    },
+    {
+        id: 15,
+        team: "Spurs",
+        w: 11,
+        l: 47
+    },
+]
