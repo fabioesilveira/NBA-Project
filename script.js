b3e4b4234c50e84fe10155aa1ca170a6881e1912
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-02 12:39:16 -0800

    addedStyle

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ for (const element of easternConference) {
    easternTeams.innerHTML += `
     <img class = "easternConference" src="${element.url}">
     <h3>${element.team}</h3>
-    <p>Stadium: ${element.stadium}</p>
-    <p>Starting Lineup: ${element.currentlyPlayers.map(e => " " + e)}</p>
+    <p class="text-primary">Stadium: ${element.stadium}</p>
+    <p class="text-danger">Starting Lineup: ${element.currentlyPlayers.map(e => " " + e)}</p>
     <p class="text-primary">NBA titles: ${element.titles.nbaTitles}</p>
-    <p>Based: ${element.based}</p>
+    <p class="text-danger">Based: ${element.based}</p>
    `
 };
 
@@ -19,10 +19,10 @@ for (const element of westernConference) {
     westernTeams.innerHTML += `
     <img class = "westernConference" src="${element.url}">
     <h3>${element.team}</h3>
-    <p>Stadium: ${element.stadium}</p>
-    <p>Starting Lineup: ${element.currentlyPlayers.map(e => " " + e)}</p>
-    <p>NBA titles: ${element.titles.nbaTitles}</p>
-    <p>Based: ${element.based}</p>
+    <p class="text-primary">Stadium: ${element.stadium}</p>
+    <p class="text-danger">Starting Lineup: ${element.currentlyPlayers.map(e => " " + e)}</p>
+    <p class="text-primary">NBA titles: ${element.titles.nbaTitles}</p>
+    <p class="text-danger">Based: ${element.based}</p>
     `
     
 };
