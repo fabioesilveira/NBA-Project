27a5e5b80354d4d3def22008f89bf9236a914f12
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-09 22:11:02 -0800

    addedQuestions

---
+++ scripts/data3.js
@@ -0,0 +1,95 @@
+const questions = [
+    {
+        q: "Which conference has won more nba championships?",
+        a: [{ text: 'Northern', isCorrect: false },
+        { text: 'Southern Conference', isCorrect: false },
+        { text: 'Wester Conference', isCorrect: false },
+        { text: 'Eastern Conference', isCorrect: true }
+        ]
+    },
+
+    {
+        q: "How many games each team get to play for the regular season?",
+        a: [{ text: '82', isCorrect: true },
+        { text: '81', isCorrect: false },
+        { text: '80', isCorrect: false },
+        { text: '90', isCorrect: false }
+        ]
+    },
+
+    {
+
+        q: "How many teams the NBA is composed of?",
+        a: [{ text: '25', isCorrect: false },
+        { text: '30', isCorrect: true },
+        { text: '20', isCorrect: false },
+        { text: '40', isCorrect: false }
+        ]
+    },
+
+    {
+        q: "Which of the following teams has not won any championships",
+        a: [{ text: 'Wizards', isCorrect: false },
+        { text: 'Suns', isCorrect: true },
+        { text: 'Warriors', isCorrect: false },
+        { text: 'Kings', isCorrect: false }
+        ]
+    },
+
+
+    {
+        q: "When and where was NBA founded?",
+        a: [{ text: '1950 / Boston-Massachussets ', isCorrect: false },
+        { text: '1932/ Los Angeles-California', isCorrect: false },
+        { text: '1946/ New York City-New Work', isCorrect: true },
+        { text: 'Toronto-Canada', isCorrect: false }
+        ]
+    },
+
+    {
+        q: "Which of the following 2 teams are tied up for the most NBA championships with 17 each",
+        a: [{ text: 'Celtics, Warriors', isCorrect: false },
+        { text: 'Lakers, Nets', isCorrect: false },
+        { text: 'Warriors, Grizzlies', isCorrect: false },
+        { text: 'Celtics, Lakers', isCorrect: true }
+        ]
+    },
+
+    {
+        q: "Which operator is used to assign a value to a variable?",
+        a: [{ text: 'X', isCorrect: false },
+        { text: '-', isCorrect: false },
+        { text: '=', isCorrect: true },
+        { text: '&', isCorrect: false }
+        ]
+    },
+
+    {
+        q: "How can you add a comment in a JavaScript?",
+        a: [{ text: '<!--This is a comment-->', isCorrect: false },
+        { text: '"This is a comment"', isCorrect: false },
+        { text: '*This is a comment*', isCorrect: false },
+        { text: '//This is a comment', isCorrect: true }
+        ]
+    },
+
+    {
+        q: "How do you declare a JavaScript variable?",
+        a: [{ text: 'var studentName;', isCorrect: true },
+        { text: 'v studentName;', isCorrect: false },
+        { text: 'variable studentName;', isCorrect: false },
+        { text: 'var <studentName>;', isCorrect: false }
+        ]
+    },
+
+    {
+        q: "What will the following code return: Boolean(10 > 9)",
+        a: [{ text: 'NaN', isCorrect: false },
+        { text: 'false', isCorrect: false },
+        { text: 'true', isCorrect: true },
+        { text: 'Undefined', isCorrect: false }
+        ]
+    },
+
+
+];
