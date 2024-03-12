dce43660a0e31c34801f3c6a9b524638ff9e6951
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-11 22:18:56 -0700

    AddedDataBaseLegends

---
+++ scripts/data4.js
@@ -0,0 +1,61 @@
+const nbaLegends = [
+    {
+        name: "Michael Jordan",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/hlm3c8zsmabyea3alpzz/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Larry Bird",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/t7va3tzjwf48uo8nlztl/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Kareem Abdul-Jabbar",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/bsplxtfn8azse418xm6i/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Magic Johnson",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/posa3g8vy6jjjegdodrh/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Kobe Bryant",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/dkzek7udmqb89d6aoaxy/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Shaquille O’Neal",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/x42v7bgq4jpdphsdfzum/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Wilt Chamberlain",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/qkfplso4mgb3p2jb5fms/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Tim Duncan",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/tfnuct1ccx2uwkydesgs/tim-duncan.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Karl Malone",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/fgt1feay3woh05oedz6w/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Julius Erving",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRv/bqnmuwftinn5jjgqkl94/best-nba-players-ever.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Bill Russell",
+        url: "https://img.buzzfeed.com/buzzfeed-static/images/ZmxfbG9zc3kscV9hdXRvL2NfY3JvcCxoXzgwOCx3XzE0MzYseF8wLHlfMA==/at6gosdktkxeehc1srdj/bill-russell.jpeg.jpeg?downsize=700:*&output-format=auto&output-quality=auto"
+    },
+
+    {
+        name: "Scottie Pippen",
+        url: "https://img.buzzfeed.com/buzzfeed-static/static/2023-06/12/18/asset/899c84e520bc/sub-buzz-1094-1686596336-1.jpg?downsize=700:*&output-format=auto&output-quality=auto"
+    }
+]
