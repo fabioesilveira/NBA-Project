a40ef200e343dad07e7699435bc2a71ebed817e3
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-15 08:54:53 -0700

    AddedFilterSearchNavBar

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## in class TODO: Search bar, Quiz, NBA Legends, Teams stats with Chart.js, function to change Teams position Based on Wins
 
-## TODO HOMEWORK NBA legends DATA BASE, TEAMS STATS WITH LOGO BUTTON FOR CHART.JS 
+## TODO HOMEWORK Try to create Legends table with map, fix style searched Teams.
 
 ## Reference 
 
--- scripts/script.js
+++ scripts/script.js
@@ -3,19 +3,43 @@ import { createElement } from "./createElement.js"
 import { createTiers } from "./createTiers.js"
 import { easternConference, westernConference, championships } from "./data.js"
 
+const newData = [...easternConference, ...westernConference]
 const westernTeams = document.getElementById("westernTeams")
 const easternTeams = document.getElementById("easternTeams")
 const searchNav = document.getElementById("search-nav")
 const nbaTiers = document.getElementById("tiers")
+const searchTeamInput = document.getElementById("searchTeam")
+const searchedTeam = document.getElementById("div-search")
+
+
+
+function searchTeam(event) {
+    event.preventDefault()
+    const value = searchTeamInput.value
+    
+    const filterTeam = newData.filter(e => e.team === value || e.stadium === value) 
+    if (filterTeam.length > 0) {
+        searchedTeam.innerHTML = ""
+        createElement(filterTeam, searchedTeam)
+        easternTeams.innerHTML = ""
+        westernTeams.innerHTML = ""
+    }
+    else {
+        alert("We couldn't find anything, try again")
+    }
+   
+    
+
+};
 
 window.addEventListener("load", () => {
     createElement(easternConference, easternTeams);
     createElement(westernConference, westernTeams);
     createTiers(championships, nbaTiers);
     createCalendar();
-})
+});
 
-searchNav.addEventListener("click", () => alert("Estou aqui"))
+searchNav.addEventListener("click", searchTeam)
 
 
 
