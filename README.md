c5d21237c523f0c99a0f1c6f5b4d9e7eb1878e42
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-16 12:13:00 -0700

    AddedStyleToHomePage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NBA-Project
 
-## in class TODO: Search bar, Quiz, NBA Legends, Teams stats with Chart.js, function to change Teams position Based on Wins
+## in class TODO:  Quiz, NBA Legends, Teams stats with Chart.js, function to change Teams position Based on Wins
 
 ## TODO HOMEWORK Try to create Legends table with map, fix style searched Teams.
 
--- scripts/createElement.js
+++ scripts/createElement.js
@@ -2,9 +2,10 @@ export function createElement(array, div) {
     array.map(element =>  div.innerHTML += `
     <img class = "easternConference" src="${element.url}">
     <h3>${element.team}</h3>
-    <p class="text-primary fw-bold">Stadium: ${element.stadium}</p>
-    <p class="text-danger fw-bold">Starting Lineup: ${element.currentlyPlayers.map(e => " " + e)}</p>
-    <p class="text-primary fw-bold">NBA titles: ${element.titles.nbaTitles}</p>
-    <p class="text-danger fw-bold">Based: ${element.based}</p>
+    <p class="text-primary fw-bold fs-5">Stadium 🏟️</p><p class="fw-bold"> ${element.stadium}</p>
+    <p class="text-danger fw-bold fs-5">Starting Lineup ⛹️⛹️⛹️⛹️⛹️</p><p class="fw-bold"> ${element.currentlyPlayers.map(e => " " + e)}</p>
+    <p class="text-primary fw-bold fs-5">NBA titles 🏆</p><p class="fw-bold"> ${element.titles.nbaTitles}</p>
+    <p class="text-primary fw-bold fs-6">Years Won 📅</p><p class=""> ${element.titles.championshipsWon}</p>
+    <p class="text-danger fw-bold fs-5">Based 📍</p><p class="fw-bold"> ${element.based}</p>
    `);       
 }
