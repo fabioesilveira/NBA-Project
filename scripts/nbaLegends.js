0862b0e404315e296c8a662cd91cb8c1cc32496a
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-17 14:52:30 -0700

    changedStyleLegends

--- scripts/nbaLegends.js
+++ scripts/nbaLegends.js
@@ -10,12 +10,12 @@ createCalendar();
 function createNbaLegends(array, div) {
     array.map(element => div.innerHTML += `
      <img class = "legends-img" src="${element.url}">
-     <h3>${element.name} ⭐</h3>
+     <h2 class="legends-name">${element.name} ⭐</h2>
      <p>${element.biography}</p>
-     <h4>Championships Won</h4>
+     <h4 class="text-primary fw-bold">Championships Won 🏆</h4>
      <p>${element.championshipsWon.times}</p>
-     <h5>Years</h5>
-     <p>${element.championshipsWon.years}</p>
+     <h5 class="text-primary fw-bold">Years Won 📅</h5>
+     <p>${element.championshipsWon.years.map(e => " " + e)}</p>
     `)
     
 }
