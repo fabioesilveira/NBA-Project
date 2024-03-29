b7b918b0cfc64ea71d5fb7346290ba372932d6fe
Fabio Silveira <fabio.eds@hotmail.com>
2024-03-29 09:12:48 -0700

    StartedQUIZcode

--- scripts/data3.js
+++ scripts/data3.js
@@ -1,93 +1,103 @@
-const questions = [
+export const questions = [
     {
         q: "Which conference has won more nba championships?",
-        a: [{ text: 'Northern', isCorrect: false },
-        { text: 'Southern Conference', isCorrect: false },
-        { text: 'Wester Conference', isCorrect: false },
-        { text: 'Eastern Conference', isCorrect: true }
+        a: [
+            { text: 'Northern', isCorrect: false },
+            { text: 'Southern Conference', isCorrect: false },
+            { text: 'Wester Conference', isCorrect: false },
+            { text: 'Eastern Conference', isCorrect: true }
         ]
     },
 
     {
         q: "How many games each team get to play for the regular season?",
-        a: [{ text: '82', isCorrect: true },
-        { text: '81', isCorrect: false },
-        { text: '80', isCorrect: false },
-        { text: '90', isCorrect: false }
+        a: [
+            { text: '82', isCorrect: true },
+            { text: '81', isCorrect: false },
+            { text: '80', isCorrect: false },
+            { text: '90', isCorrect: false }
         ]
     },
 
     {
 
         q: "How many teams the NBA is composed of?",
-        a: [{ text: '25', isCorrect: false },
-        { text: '30', isCorrect: true },
-        { text: '20', isCorrect: false },
-        { text: '40', isCorrect: false }
+        a: [
+            { text: '25', isCorrect: false },
+            { text: '30', isCorrect: true },
+            { text: '20', isCorrect: false },
+            { text: '40', isCorrect: false }
         ]
     },
 
     {
         q: "Which of the following teams has not won any championships",
-        a: [{ text: 'Wizards', isCorrect: false },
-        { text: 'Suns', isCorrect: true },
-        { text: 'Warriors', isCorrect: false },
-        { text: 'Kings', isCorrect: false }
+        a: [
+            { text: 'Wizards', isCorrect: false },
+            { text: 'Suns', isCorrect: true },
+            { text: 'Warriors', isCorrect: false },
+            { text: 'Kings', isCorrect: false }
         ]
     },
 
 
     {
         q: "When and where was NBA founded?",
-        a: [{ text: '1950 / Boston-Massachussets ', isCorrect: false },
-        { text: '1932/ Los Angeles-California', isCorrect: false },
-        { text: '1946/ New York City-New Work', isCorrect: true },
-        { text: 'Toronto-Canada', isCorrect: false }
+        a: [
+            { text: '1950 / Boston-Massachussets ', isCorrect: false },
+            { text: '1932/ Los Angeles-California', isCorrect: false },
+            { text: '1946/ New York City-New Work', isCorrect: true },
+            { text: 'Toronto-Canada', isCorrect: false }
         ]
     },
 
     {
         q: "Which of the following 2 teams are tied up for the most NBA championships with 17 each",
-        a: [{ text: 'Celtics, Warriors', isCorrect: false },
-        { text: 'Lakers, Nets', isCorrect: false },
-        { text: 'Warriors, Grizzlies', isCorrect: false },
-        { text: 'Celtics, Lakers', isCorrect: true }
+        a: [
+            { text: 'Celtics, Warriors', isCorrect: false },
+            { text: 'Lakers, Nets', isCorrect: false },
+            { text: 'Warriors, Grizzlies', isCorrect: false },
+            { text: 'Celtics, Lakers', isCorrect: true }
         ]
     },
 
     {
         q: "Which is the only Canadian team at the league?",
-        a: [{ text: 'Magics', isCorrect: false },
-        { text: 'Heat', isCorrect: false },
-        { text: 'Raptors', isCorrect: true },
-        { text: 'Pistons', isCorrect: false }
+        a: [
+            { text: 'Magics', isCorrect: false },
+            { text: 'Heat', isCorrect: false },
+            { text: 'Raptors', isCorrect: true },
+            { text: 'Pistons', isCorrect: false }
         ]
     },
 
     {
         q: "How many teams haven't won any NBA championships",
-        a: [{ text: '14', isCorrect: false },
-        { text: '9', isCorrect: false },
-        { text: '7', isCorrect: false },
-        { text: '10', isCorrect: true }
+        a: [
+            { text: '14', isCorrect: false },
+            { text: '9', isCorrect: false },
+            { text: '7', isCorrect: false },
+            { text: '10', isCorrect: true }
         ]
     },
 
     {
         q: "Which is the most popular all time player for NBA",
-        a: [{ text: 'Michael Jordan', isCorrect: true },
-        { text: 'Lebron James', isCorrect: false },
-        { text: 'Larry Bird', isCorrect: false },
-        { text: 'Shaquille O Neal', isCorrect: false }
+        a: [
+            { text: 'Michael Jordan', isCorrect: true },
+            { text: 'Lebron James', isCorrect: false },
+            { text: 'Larry Bird', isCorrect: false },
+            { text: 'Shaquille O Neal', isCorrect: false }
         ]
     },
 
     {
         q: "The winner of the NBA Finals receives:",
-        a: [{ text: 'Larry Bird Championship Trophy', isCorrect: false },
-        { text: 'Michael Jordan Championship Trophy', isCorrect: false },
-        { text: 'Larry O Brien Championship Trophy', isCorrect: true },
-        { text: 'Kobe Brynt Championship Trophy', isCorrect: false }
+        a: [
+            { text: 'Larry Bird Championship Trophy', isCorrect: false },
+            { text: 'Michael Jordan Championship Trophy', isCorrect: false },
+            { text: 'Larry O Brien Championship Trophy', isCorrect: true },
+            { text: 'Kobe Brynt Championship Trophy', isCorrect: false }
         ]
     },
 
--- scripts/nbaHistory.js
+++ scripts/nbaHistory.js
@@ -1,3 +1,38 @@
 import { createCalendar } from "./createCalendar.js";
+import { questions } from "./data3.js";
 
-createCalendar();
+const btnQuiz = document.getElementById("btn-quiz")
+const divQuiz = document.getElementById("div-quiz")
+
+window.addEventListener("load", () => {
+    createCalendar();
+});
+let score = 0
+btnQuiz.addEventListener("click", () => {
+    
+    var answer = false; 
+    
+    divQuiz.innerHTML = `
+        <h3>${questions[1].q}</h3>
+        <div>
+        ${questions[1].a.map((e, i) => `
+            <input type="radio" id="question${i}" name="questions" value="${e.isCorrect}">
+            <label for="question${i}">${e.text}</label><br>   
+        `).join("")}
+        </div>
+    `
+    divQuiz.addEventListener("change", ({ target }) => {
+        
+        answer = target.value
+
+        
+        if (answer === "true") {
+            score += 1
+            console.log(answer)
+        }
+        
+        console.log(score)
+    })
+    
+})
+
