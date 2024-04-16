078f8088eff92e586ae0c867453a6df45fee76c4
Fabio Silveira <fabio.eds@hotmail.com>
2024-04-15 22:21:17 -0700

    AddedChart

--- scripts/pages/regularEastern.js
+++ scripts/pages/regularEastern.js
@@ -4,6 +4,26 @@ import { easternRegularSeason } from "../data/data2.js";
 
 const tableEastern = document.getElementById("tableEastern")
 var dataEastern = localStorage.getItem("dataEastern")
+const myChart = document.getElementById("myChart").getContext('2d');
+
+const statsChart = new Chart(myChart, {
+    type: 'bar',
+    data: {
+        labels:["WINS", "LOSTS", "GAMES"],
+        datasets:[{
+            label:'Regular Season',
+            data:[60, 20, 82],
+            backgroundColor: 'rgb(36, 36, 142)',
+            borderWidth: 3,
+            borderColor: 'black',
+            hoverBorderWidth: 4,
+            hoverBorderColor: 'red'
+
+        }]
+    },
+    options: {}
+});
+
 
 function app() {
     if(!dataEastern) {
