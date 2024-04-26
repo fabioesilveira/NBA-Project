fc579ca024f7ecf106e6ffc58ea120339633d0a4
Fabio Silveira <fabio.eds@hotmail.com>
2024-04-26 09:02:21 -0700

    updatedTableLogicMatchs

--- scripts/functions/createTable.js
+++ scripts/functions/createTable.js
@@ -1,7 +1,7 @@
 export function createTable(array, table) {
-    array.map(element => table.innerHTML += `
+    array.map((element, index) => table.innerHTML += `
     <tr>
-        <td>${element.id}</td>
+        <td>${index + 1}</td>
         <td><img class="easternConference2" src="${element.url}">${element.team}</td>
         <td>${element.w}</td>
         <td>${element.l}</td>
--- scripts/pages/regularEastern.js
+++ scripts/pages/regularEastern.js
@@ -5,40 +5,45 @@ import { easternRegularSeason, easternStatsRegSeason } from "../data/data2.js";
 const tableEastern = document.getElementById("tableEastern");
 let dataEastern = localStorage.getItem("dataEastern");
 const btnsChart = document.querySelectorAll(".btn-team");
-const divCanvas = document.getElementById("div-canvas");
+const selectHome = document.getElementById("select-home");
+const selectAway = document.getElementById("select-away");
+const inputHome = document.getElementById("input-home");
+const inputAway = document.getElementById("input-away");
+const btnSubmit = document.getElementById("btn-submit");
 let chartInit;
 
 function createChart(data) {
-    var myChart = document.getElementById("myChart").getContext('2d');
-    var statsChart = new Chart(myChart, {
+    const myChart = document.getElementById("myChart").getContext('2d');
+    const statsChart = new Chart(myChart, {
         type: 'bar',
         data: {
-            labels:["POINTS", "3POINTS", "HOME WINS", "AWAY WINS"],
-            datasets:[{
-                label:'Regular Season',
+            labels: ["POINTS", "3POINTS", "HOME WINS", "AWAY WINS"],
+            datasets: [{
+                label: 'Regular Season',
                 data,
                 backgroundColor: 'rgb(36, 36, 142)',
                 borderWidth: 3,
                 borderColor: 'black',
                 hoverBorderWidth: 4,
                 hoverBorderColor: 'red'
-    
+
             }]
         },
         options: {}
-    }); 
+    });
     return statsChart;
 };
 
 function app() {
-    if(!dataEastern) {
+    if (!dataEastern) {
         localStorage.setItem("dataEastern", JSON.stringify(easternRegularSeason))
     };
 
     const data = JSON.parse(localStorage.getItem("dataEastern"))
     const dataChartInit = [easternStatsRegSeason[0].points, easternStatsRegSeason[0].threePoints, easternStatsRegSeason[0].homeWins, easternStatsRegSeason[0].awayWins];
-
-    createTable(data, tableEastern);
+    const dataRanking = data.sort((a, b) => b.w - a.w)
+ console.log(dataRanking)
+    createTable(dataRanking, tableEastern);
     chartInit = createChart(dataChartInit);
     createCalendar();
 };
@@ -48,8 +53,41 @@ window.addEventListener("load", app());
 btnsChart.forEach((e, i) => {
     e.addEventListener("click", () => {
         const dataChartInit = [easternStatsRegSeason[i].points, easternStatsRegSeason[i].threePoints, easternStatsRegSeason[i].homeWins, easternStatsRegSeason[i].awayWins];
-         
+
         chartInit.destroy();
         chartInit = createChart(dataChartInit);
     });
 });
+
+btnSubmit.addEventListener("click", () => {
+    console.log(inputHome.value, selectHome.value)
+if (selectHome.value === selectAway.value) {
+    return alert("Please, select different teams")
+}
+if (inputHome.value === inputAway.value) {
+    return alert("Teams can't be tied up")
+}
+    const data = JSON.parse(localStorage.getItem("dataEastern"))
+    data.forEach(element => {
+        if (selectHome.value === element.team) {
+            if (inputHome.value > inputAway.value) {
+                element.g += 1;
+                element.w += 1;
+            } else {
+                element.g += 1;
+                element.l += 1;
+            }
+        };
+        if (selectAway.value === element.team) {
+            if (inputAway.value > inputHome.value) {
+                element.g += 1;
+                element.w += 1;
+            } else {
+                element.g += 1;
+                element.l += 1;
+            }
+        }
+    });
+    localStorage.setItem("dataEastern", JSON.stringify(data))
+    return window.location.reload()
+})
